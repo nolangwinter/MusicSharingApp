dc116b9496bc994295793a5535670e34908a98ec
Nolan Winter <nolangwinter@gmail.com>
2023-10-05 20:48:28 -0700

    Connected Spotify with the App
    the user signs in with their spotify account and agrees to allow the app to use their data from their spotify account.

--- screens/LoginPage.js
+++ screens/LoginPage.js
@@ -1,10 +1,60 @@
 import { StyleSheet, Text, View, SafeAreaView, Pressable } from 'react-native'
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useNavigation } from '@react-navigation/native'
 import { Entypo } from '@expo/vector-icons';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import * as AppAuth from 'expo-app-auth'
 
 const LoginPage = () => {
   const navigation = useNavigation();
+
+  useEffect(() => {
+    const checkTokenValidity = async () => {
+      const accessToken = await AsyncStorage.getItem("token");
+      const expirationDate = await AsyncStorage.getItem("expirationDate");
+      console.log("acess token",accessToken);
+      console.log("expiration date",expirationDate);
+
+      if(accessToken && expirationDate){
+        const currentTime = Date.now();
+        if(currentTime < parseInt(expirationDate)){
+          // here the token is still valid
+          navigation.replace("Main");
+        } else {
+          // token would be expired so we need to remove it from the async storage
+          AsyncStorage.removeItem("token");
+          AsyncStorage.removeItem("expirationDate");
+        }
+      }
+    }
+
+    checkTokenValidity();
+  },[])
+  async function authenticate ()  {
+    const config = {
+      issuer:"https://accounts.spotify.com",
+      clientId:"fd58139e33ac46d38a68c93bdfda2988",
+      scopes: [
+        "user-read-email",
+        "user-library-read",
+        "user-read-recently-played",
+        "user-top-read",
+        "playlist-read-private",
+        "playlist-read-collaborative",
+        "playlist-modify-public" // or "playlist-modify-private"
+      ],
+      redirectUrl:"exp://localhost:8081/--/spotify-auth-callback"
+    }
+    const result = await AppAuth.authAsync(config);
+    console.log(result);
+    if(result.accessToken){
+      const expirationDate = new Date(result.accessTokenExpirationDate).getTime();
+      AsyncStorage.setItem("token",result.accessToken);
+      AsyncStorage.setItem("expirationDate",expirationDate.toString());
+      navigation.navigate("Main")
+    }
+  }
+
   return (
     <SafeAreaView>
       <View style={{height:80}}/>
@@ -17,7 +67,7 @@ const LoginPage = () => {
 
       <View style={{height:80}}/>
 
-      <Pressable style={styles.spotifySignIn} onPress={() => {navigation.navigate("Main")}}>
+      <Pressable style={styles.spotifySignIn} onPress={authenticate}>
         <Entypo style={styles.spotifyLogo} name="spotify" size={30}/>
         <Text style={styles.spSignInText}>
           Sign In With Spotify
